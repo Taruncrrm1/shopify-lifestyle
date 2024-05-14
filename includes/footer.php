55ae1fd2d6e1c778bb9bb1cce302e3b7bdbd98eb
taruncrrm <taruncrrm@gmail.com>
2024-05-14 22:10:04 +0530

    ecommerce site

---
+++ includes/footer.php
@@ -0,0 +1,4 @@
+<footer class="footer">
+    <div class="container text-center"><span class="text-muted"><b>Copyright&copy;Shopify Lifestyle | All Rights Reserved | Contact Us: +91 8328995745</b></span></div>
+</footer>
+    
